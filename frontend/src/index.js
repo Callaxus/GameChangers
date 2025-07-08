78765ff222cb36fa69de6b6bbb2913019e650d06
Bytes4Future <bytes4future@AKFPTSNTBY03392.local>
2025-07-08 15:37:48 +0100

    Reformolar Projecto FrontEnd - Routes e Tailwind

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './components/LoginPage';
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/team",
+    element: <LoginPage/>,
+  },
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
