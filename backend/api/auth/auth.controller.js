7758b2927c399f7957c66814de04fde26cad2b12
Callax <pedro.ferreira_farinha@outlook.pt>
2025-07-09 16:10:10 +0100

    initeidade authentication api monteiro will finish

---
+++ backend/api/auth/auth.controller.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  _id: {type: String, required: true, unique: true},
+  username: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  local:{type: String, required: true }
+});
+
+module.exports=mongoose.model('User',UserSchema);
+
+/*
+{
+    "_id": "U000",
+    "username": "username"
+    ,"password": "password",
+    "email": "mail.mail@mail.mail",
+    "local": "local"
+}
+    
+*/
