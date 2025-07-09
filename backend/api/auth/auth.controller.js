dbbb3815a55805f68435a9596354b22148ea0a3f
Callax <pedro.ferreira_farinha@outlook.pt>
2025-07-09 16:24:26 +0100

    corrigi a trizteza que fiz, monteiro needs to finish up

--- backend/api/auth/auth.controller.js
+++ backend/api/auth/auth.controller.js
@@ -1,22 +1,57 @@
-const mongoose = require('mongoose');
+// é isto 
+const User = require('../../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const UserSchema = new mongoose.Schema({
-  _id: {type: String, required: true, unique: true},
-  username: {type: String, required: true},
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  local:{type: String, required: true }
-});
+exports.register = async (req, res) => {
+  const { _id, username, email, password, local } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ msg: 'User already exists' });
 
-module.exports=mongoose.model('User',UserSchema);
+    user = new User({ _id, username, email, password, local });
 
-/*
-{
-    "_id": "U000",
-    "username": "username"
-    ,"password": "password",
-    "email": "mail.mail@mail.mail",
-    "local": "local"
-}
-    
-*/
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const payload = { user: { id: user._id } };
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'Invalid Credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid Credentials' });
+
+    const payload = { user: { id: user._id } };
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
