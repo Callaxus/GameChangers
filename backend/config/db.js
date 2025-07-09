e0bcdbf7b1bfc69db861995500a418d6e5ba49a2
Callax <pedro.ferreira_farinha@outlook.pt>
2025-07-09 12:40:59 +0100

    minor error

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      db,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
+    await mongoose.connect(db);
 
     console.log('MongoDB is Connected...');
   } catch (err) {
