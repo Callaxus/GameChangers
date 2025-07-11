53f81d682c121a5bbc86629fb2962ae8456172e9
Bytes4Future <bytes4future@AKFPTSNTBY02821.local>
2025-07-11 11:58:43 +0100

    API user profile

--- backend/api/auth/auth.middleware.js
+++ backend/api/auth/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization');
 
-  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+  if (!token) return res.status(401).json({ msg: 'Sem token, acesso negado' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');  // Summon Connection
-
+const userRoutes = require('./routes/api/user');
+app.use('/api/user', userRoutes);
 connectDB();  // Establish Connection
 
 const app = express();
