89b38fa5418ba8d640e6f550d5f9195f1792a281
Callax <pedro.ferreira_farinha@outlook.pt>
2025-07-13 23:03:50 +0100

    added some testing params in some api, its a secret but its important to add new admins into the data base with hashed passwords and all

--- backend/api/auth/auth.controller.js
+++ backend/api/auth/auth.controller.js
@@ -10,7 +10,8 @@ const jwtExpiresIn = process.env.JWT_EXPIRES_IN;
 exports.register = async (req, res) => {
   console.log('Incoming register req.body:', req.body);
 
-  const { userid, username, email, password, local, phone } = req.body;
+  const { userid, username, email, password, local, phone, role } = req.body;
+  // remove role before final release, its meant for testing only
 
   if (!phone) {
     return res.status(400).json({ msg: 'Phone number is required' });
@@ -20,14 +21,14 @@ exports.register = async (req, res) => {
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: 'User already exists' });
 
-    user = new User({ userid, username, email, password, local, phone });
-
+    user = new User({ userid, username, email, password, local, phone, role });
+// the role field is supposed to be removes before production, its merely for testing purpuses
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
 
-    const payload = { user: { id: user._id } }; // ✅ Use _id
+    const payload = { user: { id: user._id } }; //  Use _id
     jwt.sign(
       payload,
       jwtSecret,
